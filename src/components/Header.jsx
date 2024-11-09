27b8c542a8e824f2a3e44f0b5f6c68caf6f3b5c7
Salman <salmaniqbal00786@gmail.com>
2024-11-09 00:50:54 -0600

    adding more code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import logo from '../assets/logo.png';
 import  "../components/Header.css"
 
 export default function Header() {
+  let test = 5 ;
+  let check =  test > 5 ? "A" : "B"
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1 style={{backgroundColor:"red"}}>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p>A community of artists and art-lovers. {check}</p>
     </header>
   );
 }
