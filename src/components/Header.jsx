57a0f4fa361bc6e0b9fc42cdce393955ade9eeb1
Salman <salmaniqbal00786@gmail.com>
2024-11-17 23:15:28 -0600

    adding tailwind classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
 import logo from '../assets/logo.png';
-import classes from "../components/Header.module.css"
+
 
 export default function Header() {
-  let test = 5 ;
-  let check =  test > 5 ? "A" : "B"
+ 
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p className={classes.paragraph}>A community of artists and art-lovers. {check}</p>
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
+      <img src={logo} alt="A canvas" className='mb-8 w-44 h-44 object-contain'/>
+      <h1 className='text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>ReactArt</h1>
+      <p className='text-stone-500 '>A community of artists and art-lovers.</p>
     </header>
   );
 }
