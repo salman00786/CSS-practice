532b3233a28eadc8bd00acad1a0e6e23c5621ea5
Salman <salmaniqbal00786@gmail.com>
2024-11-17 23:36:09 -0600

    setting up buttons classes

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -4,10 +4,7 @@ import Button from "./Button";
 import Input from './Input';
 
 
-const ControlContainer = styled.div`display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem`;
+
 
 
 export default function AuthInputs() {
@@ -31,8 +28,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
 
           <Input
           label="Email"
@@ -53,13 +50,13 @@ export default function AuthInputs() {
             }
           />
        
-      </ControlContainer>
+      </div>
      
-      <div className="actions">
-        <Button type="button" className="text-button">
-          Create a new account
-        </Button>
-        <Button className='button' onClick={handleLogin}>Sign In</Button>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
+          Create a new account 
+        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
