3dac1079dd520a28a1cf7d8392195d66d91c0c10
Salman <salmaniqbal00786@gmail.com>
2024-11-17 23:26:22 -0600

    adding more classes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,15 +3,20 @@
 
 
 export default function Input({label, invalid, ...props}){
-    let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300";
+    let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
+    let inputClasses = "w-full px-3 py-2 leading-tight border rounded shadow"
 
-    if(invalid){
-        labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase text-red-400"
+    if(!invalid){
+        labelClasses += " text-stone-300"
+        inputClasses += "text-gray-700 bg-stone-300"
+    } else {
+        labelClasses += " text-red-400"
+        inputClasses += "text-red-400 bg-red-100 border-red-300"
     }
     return (
     <>
     <label className={ labelClasses } >{label}</label>
-    <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300" {...props}/>
+    <input className={inputClasses}{...props}/>
     </>
     )
 }
